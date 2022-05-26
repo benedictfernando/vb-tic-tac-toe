b060ad274d28b5df9bb74b7160ac528b42274972
benedictfernando <fernandobenedict619@gmail.com>
2022-05-26 22:11:10 +0800

    setup main buttons on middle of board

--- vb-tic-tac-toe/Form1.Designer.vb
+++ vb-tic-tac-toe/Form1.Designer.vb
@@ -25,6 +25,15 @@ Partial Class Form1
         Me.mainTable = New System.Windows.Forms.TableLayoutPanel()
         Me.scoreBoard = New System.Windows.Forms.TableLayoutPanel()
         Me.gameOptions = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
         Me.mainTable.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -38,6 +47,15 @@ Partial Class Form1
         Me.mainTable.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
         Me.mainTable.Controls.Add(Me.scoreBoard, 0, 0)
         Me.mainTable.Controls.Add(Me.gameOptions, 0, 4)
+        Me.mainTable.Controls.Add(Me.Button1, 1, 1)
+        Me.mainTable.Controls.Add(Me.Button2, 2, 1)
+        Me.mainTable.Controls.Add(Me.Button3, 3, 1)
+        Me.mainTable.Controls.Add(Me.Button4, 1, 2)
+        Me.mainTable.Controls.Add(Me.Button5, 2, 2)
+        Me.mainTable.Controls.Add(Me.Button6, 3, 2)
+        Me.mainTable.Controls.Add(Me.Button7, 1, 3)
+        Me.mainTable.Controls.Add(Me.Button8, 2, 3)
+        Me.mainTable.Controls.Add(Me.Button9, 3, 3)
         Me.mainTable.Dock = System.Windows.Forms.DockStyle.Fill
         Me.mainTable.Location = New System.Drawing.Point(0, 0)
         Me.mainTable.Name = "mainTable"
@@ -47,6 +65,7 @@ Partial Class Form1
         Me.mainTable.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
         Me.mainTable.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
         Me.mainTable.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5!))
+        Me.mainTable.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
         Me.mainTable.Size = New System.Drawing.Size(480, 457)
         Me.mainTable.TabIndex = 0
         '
@@ -84,6 +103,96 @@ Partial Class Form1
         Me.gameOptions.Size = New System.Drawing.Size(474, 52)
         Me.gameOptions.TabIndex = 1
         '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button1.Location = New System.Drawing.Point(63, 60)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 108)
+        Me.Button1.TabIndex = 2
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button2.Location = New System.Drawing.Point(183, 60)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 108)
+        Me.Button2.TabIndex = 3
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button3.Location = New System.Drawing.Point(303, 60)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(114, 108)
+        Me.Button3.TabIndex = 4
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button4.Location = New System.Drawing.Point(63, 174)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(114, 108)
+        Me.Button4.TabIndex = 5
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button5.Location = New System.Drawing.Point(183, 174)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(114, 108)
+        Me.Button5.TabIndex = 6
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button6.Location = New System.Drawing.Point(303, 174)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(114, 108)
+        Me.Button6.TabIndex = 7
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button7.Location = New System.Drawing.Point(63, 288)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(114, 108)
+        Me.Button7.TabIndex = 8
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button8.Location = New System.Drawing.Point(183, 288)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(114, 108)
+        Me.Button8.TabIndex = 9
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button9.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button9.Location = New System.Drawing.Point(303, 288)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(114, 108)
+        Me.Button9.TabIndex = 10
+        Me.Button9.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 21.0!)
@@ -105,4 +214,13 @@ Partial Class Form1
     Friend WithEvents mainTable As TableLayoutPanel
     Friend WithEvents scoreBoard As TableLayoutPanel
     Friend WithEvents gameOptions As TableLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
 End Class
