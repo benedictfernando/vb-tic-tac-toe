0cd822ed31d9d2e627ccbd71d7123f2a2a80d17c
benedictfernando <fernandobenedict619@gmail.com>
2022-05-27 00:11:41 +0800

    setup winning combinations

--- vb-tic-tac-toe/Form1.vb
+++ vb-tic-tac-toe/Form1.vb
@@ -26,4 +26,43 @@
             ' checkForWinner()
         End If
     End Sub
+
+    Private Sub checkForWinner()
+        Dim result As String = Nothing
+
+        Dim tl As String = Me.tl.Text,
+            t As String = Me.t.Text,
+            tr As String = Me.tr.Text,
+            l As String = Me.l.Text,
+            m As String = Me.m.Text,
+            r As String = Me.r.Text,
+            bl As String = Me.bl.Text,
+            b As String = Me.b.Text,
+            br As String = Me.br.Text
+
+        ' For trios with 'middle' button as their common cell
+        If (tl = m And m = br Or t = m And m = b Or tr = m And m = bl Or r = m And m = l) And Not Me.m.Enabled Then
+            result = m
+
+            ' For trios with 'top-left' button as their common cell
+        ElseIf (tl = t And t = tr Or tl = l And l = bl) And Not Me.tl.Enabled Then
+            result = tl
+
+            ' For trios with 'bottom-right' button as their common cell
+        ElseIf (br = r And r = tr Or br = b And b = bl And Not Me.br.Enabled Then
+            result = br
+
+        End If
+
+        ' For tie situation
+        If result = Nothing Then
+            ' check for tie
+        End If
+
+        ' For results
+        If result = Not Nothing Then
+            ' add score
+        End If
+
+    End Sub
 End Class
